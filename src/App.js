9807d715d6be3fb06c76e7d87158c206d3105b53
David White <dwhite0627@gmail.com>
2020-11-12 22:25:05 -0500

    Add the Navbar, StoryList, and SearchForm components to the App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+// Component imports
+import Navbar from "./components/Navbar";
+import StoryList from "./components/StoryList";
+import SearchForm from "./components/SearchForm";
 
 function App() {
+  const [results, setResults] = useState([]); // The search results
   return (
     <div className="App">
-
+      <Navbar />
+      <SearchForm setResults={setResults} />
+      <StoryList stories={results} />
     </div>
   );
 }
