e601e6920862a69940afaa56ca515d1c4119783d
David White <dwhite0627@gmail.com>
2020-11-13 20:06:54 -0500

    Mock the redux store in index.test.js

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -23,9 +23,28 @@ jest.mock('react-dom', () => ({
   },
 }));
 
+// Mock the redux store functions
+// This allows jest.mock.toHaveBeenCalledWith to successfully compare Redux stores
+// passed to it
+function mockFunction() {
+  return {};
+}
+
+// Mock the redux store
+jest.mock('redux', () => ({
+  createStore: () => {
+    return {
+      dispatch: mockFunction,
+      getState: mockFunction,
+      replaceReducer: mockFunction,
+      subscribe: mockFunction,
+    };
+  },
+}));
+
 describe('index.js testing suite', () => {
   test('it loads the index.js page', () => {
-    // Simulate the creation and appending of the root element to the DOM
+    // Simulate creating and appending the root element to the DOM
     const root = document.createElement('div');
     root.id = 'root';
     document.body.appendChild(root);
