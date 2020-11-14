a7244f09521de265bd652e4dfbc66b5e3e76a179
David White <dwhite0627@gmail.com>
2020-11-13 20:05:42 -0500

    Add a custom React Testing Library render utility to facilitate testing with redux

---
+++ src/utils/test/testUtils.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+// RTL imports
+import { render as rtlRender } from '@testing-library/react';
+
+// Redux imports
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+// Reducer imports
+import searchReducer from '../../reducers/searchReducer';
+
+// Custom render function overrides the render function in RTL
+// It wraps tested components in the Provider component from react-redux
+function render(
+  ui,
+  {
+    initialState,
+    store = createStore(searchReducer, initialState),
+    ...renderOptions
+  } = {}
+) {
+  function Wrapper({ children }) {
+    return <Provider store={store}>{children}</Provider>;
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+// re-export everything in RTL
+export * from '@testing-library/react';
+
+// export the extended render method
+export { render };
