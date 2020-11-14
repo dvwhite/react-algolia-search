8f7e990287afe6d6a09c66a6fe5084e31bdf6556
David White <dwhite0627@gmail.com>
2020-11-14 18:13:44 -0500

    Refactor replaceStoryText to make it more readable

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,20 +1,29 @@
 /**
  * Parse a string containing unicode characters
  * @param {string} strToParse: The string to parse from Unicode
- * @returns {string} 
+ * @returns {string}
  */
 function replaceStoryText(strToParse) {
   // Replace <p> text and unicode equivalent with a break element
   const pCharText = '<p>'; // Signifies a line feed char
   const pCharUnicode = '&#60;p&#62;'; // The same line feed char in unicode
-  const lineBreaks = '<br /><br />'
+  const lineBreaks = '<br /><br />';
 
   // Using string.split.join is compatible with older browsers
   if (strToParse) {
-    strToParse = strToParse.split(pCharText).join(lineBreaks);
-    strToParse = strToParse.split(pCharUnicode).join(lineBreaks);
+    // An object containing each origin character and what to replace it with
+    const replacements = {
+      [pCharText]: lineBreaks,
+      [pCharUnicode]: lineBreaks,
+    };
+
+    // Replace each character in the replacements object
+    for (let key in replacements) {
+      strToParse = strToParse.split(key).join(replacements[key]);
+    }
+
     return strToParse;
   }
 }
 
-export { replaceStoryText }
+export { replaceStoryText };
